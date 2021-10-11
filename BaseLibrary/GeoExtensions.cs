7ca5c9fd807261a2da7d066753b8c28e262de9e7
Karen Payne <Karen.1.Payne@oregon.gov>
2021-10-11 10:28:13 -0700

    started unit testing project

--- BaseLibrary/GeoExtensions.cs
+++ BaseLibrary/GeoExtensions.cs
@@ -13,12 +13,15 @@ namespace BaseLibrary
         /// </summary>
         /// <param name="value">double value to validate</param>
         /// <returns>true if valid, false if not valid</returns>
-        public static bool IsLatitude(this double value) => value  <= 90.0 &&  value >= -90.0;
+        public static bool IsLatitude(this double value) 
+            => value  <= 90.0 &&  value >= -90.0;
+
         /// <summary>
         /// Validate sender is a valid longitude
         /// </summary>
         /// <param name="value">double value to validate</param>
         /// <returns>true if valid, false if not valid</returns>
-        public static bool IsLongitude(this double value) => value  <= 180.0 && value >= -180.0;
+        public static bool IsLongitude(this double value) 
+            => value  <= 180.0 && value >= -180.0;
     }
 }
